60e04b51b44a0d9b831354a5a53fc37c3b66a585
lautarofz7 <lautarolucianofernandez@gmail.com>
2026-01-28 12:04:19 -0300

    2.3.1b

--- LISTAINFO.cpp
+++ LISTAINFO.cpp
@@ -1,6 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h> 
-#include <conio.h> 
-#include <string.h>
+#include <conio.h>
+#include "CAMPOS.h" 
+
+void realizarListaInfo()
+{
+    struct registro r; 
+    FILE *archivo;
+    int cont = 0;
+
+    system("CLS");
+    printf("=== LISTADO GENERAL DE EMPLEADOS ===\n\n");
+
+   
+    archivo = fopen("campos.dat", "rb");
+
+   
+    if (archivo == NULL)
+    {
+        printf("[ERROR]: No se pudo abrir el archivo 'campos.dat'.\n");
+        printf("Causas probables:\n");
+        printf(" - No has cargado ningun empleado aun.\n");
+        printf(" - El archivo tiene otro nombre en la funcion de Alta.\n");
+        printf("\nPresione una tecla para volver al menu...");
+        getch();
+        return;  
+    }
+
+   
+    printf("%-10s %-25s %-5s %-10s %-15s\n", "LEGAJO", "NOMBRE", "EDAD", "SUELDO", "PUESTO");
+    printf("--------------------------------------------------------------------------\n");
+
+   
+    fread(&r, sizeof(struct registro), 1, archivo);
+
+    while (!feof(archivo))
+    {
+        printf("%-10d %-25s %-5d $%-9d %-15s\n", 
+               r.legajo, 
+               r.nombreapell, 
+               r.edad, 
+               r.sueldo, 
+               r.puesto);
+        
+        cont++;
+
+        fread(&r, sizeof(struct registro), 1, archivo);
+    }
+
+  
+    fclose(archivo);
+
+  
+    if (cont == 0)
+    {
+        printf("\n[ATENCION]: El archivo existe pero esta VACIO.\n");
+    }
+    else
+    {
+        printf("\n--------------------------------------------------------------------------\n");
+        printf("Se listaron %d registros.\n", cont);
+    }
+
+    printf("\nPresione una tecla para volver al menu...");
+    getch();
+}
